5a631cd1d8ef7fa5b0ce3d51837463005f68c098
Aravinth1207 <aravinthajith2812@gmail.com>
2021-10-27 20:21:19 +0530

    2D Array - DS

--- Hourglass.py
+++ Hourglass.py
@@ -1,12 +1,13 @@
 arr = []
-C = 6
-R = 6
+
 for i in range(6):
     arr.append(list(map(int,input().split())))
-sumArray = [] 
-for i in range(0, R-2):
-    for j in range(0, C-2):
+MAX = -9999
+for i in range(0, 4):
+    for j in range(0, 4):
+        
         SUM = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2]) + (arr[i + 1][j + 1]) + (arr[i + 2][j] +  arr[i + 2][j + 1] + arr[i + 2][j + 2])
-        sumArray.append(SUM)
+        if MAX <= SUM:
+            MAX = SUM
 
-print(sumArray)
+print(MAX)
